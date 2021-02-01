eca8e0b96eed11c68dd7ac106a0e0bd1f1a862a2
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-01 11:17:11 -0500

    commit tree 8b0a06426d2543e79ce3a8a2fe39fbae8fb14989

--- spec/sinatra_logging_in_and_out_spec.rb
+++ spec/sinatra_logging_in_and_out_spec.rb
@@ -82,7 +82,7 @@ describe 'ApplicationController' do
       }
       post '/login', params
       get '/account'
-      expect(last_response.body).to include("<h1>Welcome skittles123</h1>")
+      expect(last_response.body).to include("<h2>Welcome skittles123</h2>")
       expect(last_response.body).to include("<h3>Your Balance: 1000.0</h3>")
     end
   end
